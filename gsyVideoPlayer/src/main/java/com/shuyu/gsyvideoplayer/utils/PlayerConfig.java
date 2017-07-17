1020512379e742d309e4d366ad92f8b5101816b2
blackbbc <505968815@qq.com>
2017-07-17 17:59:22 +0800

    Update player config

--- gsyVideoPlayer/src/main/java/com/shuyu/gsyvideoplayer/utils/PlayerConfig.java
+++ gsyVideoPlayer/src/main/java/com/shuyu/gsyvideoplayer/utils/PlayerConfig.java
@@ -14,11 +14,15 @@ public class PlayerConfig {
 
     private static final String KEY_I_DANMU_OPACITY = "danmu_opacity";
     private static final String KEY_I_DANMU_SIZE = "danmu_size";
+    private static final String KEY_I_DANMU_SPEED = "danmu_speed";
     private static final String KEY_B_HARD_CODEC = "hard_codec";
+    private static final String KEY_F_PLAYER_SPEED = "player_speed";
     private static final String KEY_I_LAND_SCREEN_TYPE = "land_screen_type";
 
     private static final int DEFAULT_DANMU_OPACITY = 80;
     private static final int DEFAULT_DANMU_SIZE = 50;
+    private static final int DEFAULT_DANMU_SPEED = 70;
+    private static final float DEFAULT_PLAYER_SPEED = 1f;
     private static final boolean DEFAULT_HARD_CODEC = false;
     private static final int DEFAULT_LAND_SCREEN_TYPE = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
 
@@ -46,11 +50,35 @@ public class PlayerConfig {
         return sp.getInt(KEY_I_DANMU_SIZE, DEFAULT_DANMU_SIZE);
     }
 
+    public static void saveDanmuSpeed(int danmuSpeed) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt(KEY_I_DANMU_SPEED, danmuSpeed);
+        editor.commit();
+    }
+
+    public static int loadDanmuSpeed() {
+        return sp.getInt(KEY_I_DANMU_SPEED, DEFAULT_DANMU_SPEED);
+    }
+
+    /*
+     * Not used for now
+     */
+    public static void savePlayeSpeed(boolean playerSpeed) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean(KEY_F_PLAYER_SPEED, playerSpeed);
+        editor.commit();
+    }
+
+    public static float loadPlayerSpeed() {
+        return sp.getFloat(KEY_F_PLAYER_SPEED, DEFAULT_PLAYER_SPEED);
+    }
+
     public static void saveHardCodec(boolean hardCodec) {
         SharedPreferences.Editor editor = sp.edit();
         editor.putBoolean(KEY_B_HARD_CODEC, hardCodec);
         editor.commit();
     }
+
 
     public static boolean loadHardCodec() {
         return sp.getBoolean(KEY_B_HARD_CODEC, DEFAULT_HARD_CODEC);
